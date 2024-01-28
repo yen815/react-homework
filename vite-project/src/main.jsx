f3fe7fddd2cd6dbcb46f5aa1e644c44b008eafae
yen815 <chldpdms815@gmail.com>
2024-01-28 18:09:10 +0900

    기초구성

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// import React from "https://esm.sh/react";
+import { createRoot } from "https://esm.sh/react-dom";
+
+function bodyStyling() {
+  document.body.style.backgroundColor = `#B1D0FE`;
+  document.body.style.color = `#000`;
+}
+
+bodyStyling();
+
+/* -------------------------------------------------------------------------- */
+
+const renderKosdaqIndex = () => (
+  <div>
+    <span className="KosdaqIndex">859.65</span>
+  </div>
+);
+
+const renderKosdaqPercent = () => (
+  <div>
+    <span className="Kosdaq">코스닥</span>
+    <span className="KosdaqPercent"> ⏷ 0.97% </span>
+  </div>
+);
+
+const renderWidgetBar = () => (
+  <div className="widgetBarGroup" role="group">
+    <div className="widgetGroup">
+      {renderKosdaqIndex()}
+      {renderKosdaqPercent()}
+    </div>
+  </div>
+);
+
+createRoot(document.getElementById("root")).render(renderWidgetBar());
